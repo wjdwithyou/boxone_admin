6ede4c71759e27f4fb8649faafe7c3df33fc8c66
parkya200 <parkya200@naver.com>
2016-01-11 14:35:17 +0900

    미리보기기능 추가

--- resources/views/advertise.php
+++ resources/views/advertise.php
@@ -34,8 +34,7 @@
                      <td class="ad_num"><?= $i->idx?></td>
                      <td class="ad_pc"><?= $i->id?></td>
                      <td class="ad_board_title" style="font-weight:bold" ><a onclick='adModify(<?=$i->idx?>);'><?= $i->name?></a></td>
-					 <td class="ad_pc"><img src="<?=$adr_img ?>advertise/<?=$i->idx?>_img.png"/></td>
->>>>>>> 33c22ae911300e28a97851547e119957fc3c9603
+					 <td class="ad_img"><img width="150px" height="80px" src="<?=$adr_img ?>advertise/<?=$i->idx?>_img.png"/></td>
                      <td class="ad_pc"><?= $i->upload?></td>
                   </tr>
                   <!-- /광고목록 글 -->
