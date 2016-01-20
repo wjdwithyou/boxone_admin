539177225eaeaf071033566076950c363ef44692
parkya200 <parkya200@naver.com>
2016-01-20 10:48:29 +0900

    temp

--- resources/views/productupdate.php
+++ resources/views/productupdate.php
@@ -11,7 +11,7 @@
 	$(document).ready(function(){
    	 $("button").click(function(){
    	 	$("#loading").show();
-        if(("#loading").is(":visible"))
+        if($("#loading").is(":visible"))
         $("#test").load("<?= $adr_ctr ?>Product/insertProd");
         
     	});
